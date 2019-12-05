c5ebfd5e522de04add7f56fb09d2395865e4de21
matthewCLynn <matthewchristopherlynn@yahoo.com>
2019-12-05 16:08:18 -0500

    first commit

---
+++ HeartbeatCountDown/src/main/java/co/gc/HeartbeatCountDown/HeartbeatCountDownApplication.java
@@ -0,0 +1,13 @@
+package co.gc.HeartbeatCountDown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeatCountDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeatCountDownApplication.class, args);
+	}
+
+}
---
+++ HeartbeatCountDown/src/test/java/co/gc/HeartbeatCountDown/HeartbeatCountDownApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gc.HeartbeatCountDown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatCountDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
